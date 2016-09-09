0fc1b53d6b5556bfc06cdb4d15d90e5c0efca314
Andrew Janke <andrew@apjanke.net>
2016-09-09 19:48:32 -0400

    go-gui: Switch to Unix-style installation
    This removes the dependency on Apple's Java 1.6 that gogui.app has.

--- go-gui.rb
+++ go-gui.rb
@@ -3,6 +3,7 @@ class GoGui < Formula
   homepage "http://gogui.sourceforge.net"
   url "https://downloads.sourceforge.net/project/gogui/gogui/1.4.9/gogui-1.4.9.zip"
   sha256 "32684b756ab5b6bf9412c035594eddfd1be9250de12d348c3501850857b86662"
+  revision 1
 
   head do
     url "git://git.code.sf.net/p/gogui/code"
@@ -12,12 +13,17 @@ class GoGui < Formula
   end
 
   depends_on :ant => :build
-  depends_on :java => "1.6"
+  depends_on :java => "1.6+"
 
   resource "quaqua" do
     url "http://www.randelshofer.ch/quaqua/files/quaqua-5.4.1.nested.zip"
     sha256 "a01ce8bcce6e81941ca928468e728e76e0773957c685c349474ee04f3be677d6"
   end
+
+  # Disable Linux-specific install steps
+  # See https://github.com/Homebrew/homebrew-games/pull/598
+  # and https://sourceforge.net/p/gogui/bugs/43/
+  patch :DATA
 
   def install
     inreplace "build.xml", "/Developer/Tools/SetFile", "/usr/bin/SetFile"
@@ -35,13 +41,33 @@ class GoGui < Formula
         -Ddoc-uptodate=true
       ]
     end
-    system "ant", "gogui.app", *args
-    prefix.install "build/GoGui.app"
-    bin.write_exec_script "#{prefix}/GoGui.app/Contents/MacOS/JavaApplicationStub"
-    mv "#{bin}/JavaApplicationStub", "#{bin}/gogui"
+    # Use the Linux-style install instead of gogui.app to avoid Apple Java 1.6
+    # dependency. https://sourceforge.net/p/gogui/bugs/42/
+    system "./install.sh", "-p", prefix, "-j", "/usr"
   end
 
   test do
     assert_equal "GoGui #{version}", shell_output("#{bin}/gogui -version").chomp
   end
 end
+
+__END__
+diff --git a/install.sh b/install.sh
+index 848b399..3851420 100755
+--- a/install.sh
++++ b/install.sh
+@@ -94,6 +94,8 @@ install -m 644 config/gogui.desktop "$PREFIX/share/applications"
+ install -d "$PREFIX/share/mime/packages"
+ install -m 644 config/gogui-mime.xml "$PREFIX/share/mime/packages"
+ 
++if [ `uname` == 'linux' ]; then
++
+ # Install Gnome 2 thumbnailer
+ 
+ install -d "$SYSCONFDIR/gconf/schemas"
+@@ -126,3 +128,5 @@ update-desktop-database "$PREFIX/share/applications" >/dev/null 2>&1
+ # MIME database
+ 
+ update-mime-database "$PREFIX/share/mime" >/dev/null 2>&1
++
++fi
